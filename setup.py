51c0bbe5383093ea5f5d53c9476f7b79936b03ae
Lirian Su <liriansu@gmail.com>
2017-06-04 17:04:31 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
+
+from setuptools import setup
+
+setup(
+    name='chinesecalendar',
+    version='0.0.1',
+    description='check if some day is holiday in China',
+    author='Lirian Su',
+    author_email='liriansu@gmail.com',
+    url='https://github.com/LKI/chinese-calendar',
+    license='WTFPL',
+    packages=['chinese_calendar'],
+)
