915c579265b0b7c8f2f54cbaf0f24b41c81306b3
Lirian Su <liriansu@gmail.com>
2017-06-04 16:49:53 +0800

    add year 2016

--- chinese_calendar/constants.py
+++ chinese_calendar/constants.py
@@ -15,6 +15,22 @@ class Holiday(object):
 
 
 holidays = {
+    datetime.date(year=2016, month=1, day=1): Holiday.new_years_day,
+    datetime.date(year=2016, month=2, day=8): Holiday.spring_festival,
+    datetime.date(year=2016, month=2, day=9): Holiday.spring_festival,
+    datetime.date(year=2016, month=2, day=10): Holiday.spring_festival,
+    datetime.date(year=2016, month=2, day=11): Holiday.spring_festival,
+    datetime.date(year=2016, month=2, day=12): Holiday.spring_festival,
+    datetime.date(year=2016, month=4, day=4): Holiday.tomb_sweeping_day,
+    datetime.date(year=2016, month=5, day=2): Holiday.labour_day,
+    datetime.date(year=2016, month=6, day=9): Holiday.dragon_boat_festival,
+    datetime.date(year=2016, month=9, day=15): Holiday.mid_autumn_festival,
+    datetime.date(year=2016, month=9, day=16): Holiday.mid_autumn_festival,
+    datetime.date(year=2016, month=10, day=3): Holiday.national_day,
+    datetime.date(year=2016, month=10, day=4): Holiday.national_day,
+    datetime.date(year=2016, month=10, day=5): Holiday.national_day,
+    datetime.date(year=2016, month=10, day=6): Holiday.national_day,
+    datetime.date(year=2016, month=10, day=7): Holiday.national_day,
     datetime.date(year=2017, month=1, day=2): Holiday.new_years_day,
     datetime.date(year=2017, month=1, day=27): Holiday.spring_festival,
     datetime.date(year=2017, month=1, day=30): Holiday.spring_festival,
@@ -34,6 +50,12 @@ holidays = {
 }
 
 workdays = {
+    datetime.date(year=2016, month=2, day=6): Holiday.spring_festival,
+    datetime.date(year=2016, month=2, day=14): Holiday.spring_festival,
+    datetime.date(year=2016, month=6, day=12): Holiday.dragon_boat_festival,
+    datetime.date(year=2016, month=9, day=18): Holiday.mid_autumn_festival,
+    datetime.date(year=2016, month=10, day=8): Holiday.national_day,
+    datetime.date(year=2016, month=10, day=9): Holiday.national_day,
     datetime.date(year=2017, month=2, day=4): Holiday.spring_festival,
     datetime.date(year=2017, month=4, day=1): Holiday.tomb_sweeping_day,
     datetime.date(year=2017, month=5, day=27): Holiday.dragon_boat_festival,
