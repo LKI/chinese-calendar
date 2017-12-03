53764acfa252e4ce56203cc2fe92c2ef9f8cb29f
Lirian Su <liriansu@gmail.com>
2017-12-03 21:26:09 +0800

    feat: use pypandoc to get rst content

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
-
+import pypandoc
 from setuptools import setup
 
 setup(
     name='chinesecalendar',
     version='0.0.4',
     description='check if some day is holiday in China',
+    long_description=pypandoc.convert('README.md', 'rst'),
     author='Lirian Su',
     author_email='liriansu@gmail.com',
     url='https://github.com/LKI/chinese-calendar',
