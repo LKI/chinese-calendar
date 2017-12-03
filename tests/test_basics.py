06f947c1f8a804b5cfa785042d32c16783e91ead
Lirian Su <liriansu@gmail.com>
2017-12-03 21:29:37 +0800

    feat: add unit test

--- tests/test_basics.py
+++ tests/test_basics.py
@@ -17,6 +17,7 @@ class BasicTests(TestCase):
             datetime.date(year=2017, month=5, day=30),
             datetime.date(year=2017, month=6, day=4),
             datetime.date(year=2017, month=10, day=1),
+            datetime.date(year=2018, month=5, day=1),
         ]
         for date in dates:
             self.assertFalse(is_workday(date))
@@ -27,7 +28,12 @@ class BasicTests(TestCase):
             datetime.date(year=2017, month=5, day=27),
             datetime.date(year=2017, month=6, day=5),
             datetime.date(year=2017, month=10, day=31),
+            datetime.date(year=2018, month=5, day=10),
         ]
         for date in dates:
             self.assertFalse(is_holiday(date))
             self.assertTrue(is_workday(date))
+
+    def test_not_implemented(self):
+        with self.assertRaises(NotImplementedError):
+            is_holiday(datetime.date(year=2001, month=1, day=1))
