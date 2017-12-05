8e883ae8daae298109bceed81799b87e7b2c43ec
Lirian Su <liriansu@gmail.com>
2017-12-05 22:51:06 +0800

    feat: add flake8

--- chinese_calendar/__init__.py
+++ chinese_calendar/__init__.py
@@ -1,2 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
+
+from .utils import is_holiday, is_workday
+
+__all__ = ['is_holiday', 'is_workday']
--- chinese_calendar/utils.py
+++ chinese_calendar/utils.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
 
-import datetime
-
 from chinese_calendar.constants import holidays, workdays
 
 
