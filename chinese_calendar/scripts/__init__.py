2d398d107460ac9c93828b3c6cf3392d1e6f226e
Lirian Su <liriansu@gmail.com>
2018-01-06 21:45:56 +0800

    feat: add scripts to generate constants

---
+++ chinese_calendar/scripts/__init__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
+
+from os.path import dirname, join, realpath
+
+from chinese_calendar.scripts.data import Arrangement, constants_template
+
+
+def _get_lines(days):
+    yield '{'
+    sentence = '    datetime.date(year={}, month={}, day={}): {}.value,'
+    for date in sorted(days.keys()):
+        yield sentence.format(date.year, date.month, date.day, days[date])
+    yield '}'
+
+
+def generate_constants():
+    arrangement = Arrangement()
+    file_content = constants_template.format(
+        '\n'.join(_get_lines(arrangement.holidays)),
+        '\n'.join(_get_lines(arrangement.workdays)),
+    )
+    file_path = join(dirname(dirname(realpath(__file__))), 'constants.py')
+    with open(file_path, 'wb') as f:
+        f.write(file_content.encode('utf-8'))
+
+
+if __name__ == '__main__':
+    generate_constants()
