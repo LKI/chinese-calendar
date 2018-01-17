d6d1034a0c5eb9a8652cbe1aaca8d0baf08b6092
Lirian Su <liriansu@gmail.com>
2018-01-17 23:47:51 +0800

    Bump version: 0.1.0 → 1.0.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='chinesecalendar',
-    version='0.1.0',
+    version='1.0.0',
     description='check if some day is holiday in China',
     long_description=pypandoc.convert('README.md', 'rst'),
     author='Lirian Su',
