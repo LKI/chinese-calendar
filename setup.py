93479b763addacf827f7e654bdf63ca34f6e6261
Lirian Su <liriansu@gmail.com>
2021-10-26 11:07:55 +0800

    feat: support py310

--- setup.py
+++ setup.py
@@ -25,5 +25,6 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
 )
