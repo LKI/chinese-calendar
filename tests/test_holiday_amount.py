bbc59ed4dfab7ef4f4732a2191f2f77ee722b79d
Lirian Su <liriansu@gmail.com>
2024-11-13 13:37:14 +0800

    feat: support 2025's holiday (#120)

--- tests/test_holiday_amount.py
+++ tests/test_holiday_amount.py
@@ -21,6 +21,11 @@ class HolidayAmountTests(unittest.TestCase):
         holiday_amounts[2012] -= 1  # 12年可以享受11年补班的假
         holiday_amounts[2015] -= 1  # 15年是中国人民抗日战争暨世界反法西斯战争胜利70周年，多放1天
         holiday_amounts[2020] -= 2  # 20年春节因为新型冠状病毒疫情防控，延长假期2天
-        for year in range(2007, 2023 + 1):  # 06年数据少，不测了
-            self.assertEqual(11, holiday_amounts[year], "Holiday amount of year {}".format(year))
+        max_supported_year = max([_.year for _ in holidays])
+        for year in range(2007, max_supported_year + 1):  # 06年数据少，不测了
+            if year <= 2024:
+                expected_holidays = 11
+            else:
+                expected_holidays = 13
+            self.assertEqual(expected_holidays, holiday_amounts[year], "Holiday amount of year {}".format(year))
         self.assertEqual(1, 1)
